e6f40be26eb235ae87cf019675c26321a534eca3
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-03 17:05:36 +0300

    jwt authorization

--- controllers/main.js
+++ controllers/main.js
@@ -1,17 +1,25 @@
 const CustomAPIError = require("../errors/custom-error");
+const jwt = require('jsonwebtoken');
 
 const login = async(req,res) => {
     const {username,password} = req.body;
     if(!username || !password){
         throw new CustomAPIError('Please provide email and password',400)
     }
-    res.send('Fake Login/Register/Signup Route')
+
+    const id = new Date().getDate() // id not implemented, random one. just demo
+    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
+    res.status(200).json({msg:'user created',token})
 }
 
-const dashboard = async(req,res) =>{
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello, Flavius`,secret:`Here is your authorized data,your lucky number is ${luckyNumber}`});
-}
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100)
+  
+    res.status(200).json({
+      msg: `Hello, ${req.user.username}`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    })
+  }
 
 module.exports = {
     login,dashboard
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { login, dashboard } = require('../controllers/main');
+const authenticationMiddleware = require('../middleware/auth');
 const router = express.Router();
 
-router.get('/dashboard',dashboard);
+router.get('/dashboard',authenticationMiddleware,dashboard);
 router.post('/login',login);
 
 module.exports = router
